162dad058eaba5a9df08949f36e51c6b1573aba0
Felix550 <felix16072010@libero.it>
2025-06-25 18:52:26 +0200

    port

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app.get('/', (req, res) => {
     });
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 
 const server = app.listen(PORT, () => {
     console.log("server is running on port", server.address().port);
